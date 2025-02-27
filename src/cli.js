5f6362dca50740bed7890536c2e9e303d2737d08
unknown <sonya888kykyvna0@gmail.com>
2025-02-27 13:38:59 +0700

    five

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const name = readlineSync.question('Your answer: ');
+const gro = () => {
+const name = readlineSync.question('May I have your name? John');
+console.log('Hello, John!')
+}
+export default gro
