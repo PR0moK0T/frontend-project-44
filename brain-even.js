7d9e3e36fa280178da5f982dcee7e3145b66cd77
PR0moK0T <sonya888kykyvna0@gmail.com>
2025-03-05 17:07:08 +0000

    six

--- brain-even.js
+++
@@ -1,4 +0,0 @@
-import gro from "../index.js";
-console.log('Welcome to the Brain Games!');
-gro()
-console.log('Answer "yes" if the number is even, otherwise answer "no".')
--- brain-games.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import gro from "../index.js";
-console.log('Welcome to the Brain Games!');
-gro()
