c43cd6c8df209164eb9e672f8d23905f45bc19a4
PR0moK0T <sonya888kykyvna0@gmail.com>
2025-03-07 15:55:24 +0700

    0.9

---
+++ bin/brain-prime.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+import { gro, getRandomIntInclusive } from "../index.js";
+
+let y = 0
+
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num === 2) return true;
+    if (num % 2 === 0) return false; 
+
+    for (let i = 3; i <= Math.sqrt(num); i += 2) { 
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+let yousername = gro()
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
+
+while (y < 3) {
+    const number = getRandomIntInclusive(1, 100);
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    const correctAnswer = isPrime(number) ? 'yes' : 'no';
+
+    if (userAnswer === correctAnswer) {
+        console.log('Correct!');
+    } else {
+        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, ${yousername}!`);
+        break;
+    }
+    y++
+    if (y === 3) { console.log(`Congratulations, ${yousername}`) }
+}
