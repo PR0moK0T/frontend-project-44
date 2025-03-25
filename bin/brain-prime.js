6cd44d8c815a45da9ab43d057a9f90e09f21a15e
PR0moK0T <sonya888kykyvna0@gmail.com>
2025-03-25 22:40:52 +0000

    brain prime bag fix

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -13,7 +13,7 @@ function isPrime(num) {
   if (num === 2) return true;
   if (num % 2 === 0) return false;
 
-  for (let i = 3; i <= Math.sqrt(num); i += 2) {
+  for (let i = 3; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) return false;
   }
   return true;
